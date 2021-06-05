cdf061add8c026b798a5c71cf2731fe0523e103d
qthang <quangthangdk95@gmail.com>
2021-06-05 22:55:15 +0700

    Update levelmaxblock.js

--- levelmaxblock.js
+++ levelmaxblock.js
@@ -26,7 +26,18 @@ function testImage(URL) {
   tester.src=URL;
 }
 function imageFound() {
-    console.log('Cảm ơn vì không chặn quảng cáo tại QThang.net');
+    //console.log('không chặn');
+  var b = document.createElement("script");
+  b.type = "text/javascript";
+  b.async = !0;
+  b.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  b.onerror = function() {
+    f();
+    window.adblock = !0
+  };
+  
+  var e = document.getElementsByTagName("script")[0];
+  e.parentNode.insertBefore(b, e);
 }
 
 function imageNotFound() {
